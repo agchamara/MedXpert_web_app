96979008913a44689450f16b151c4c990e6c7f8d
agchamara <101026270+agchamara@users.noreply.github.com>
2022-06-06 10:45:24 -0700

    Add files via upload

---
+++ cardioModel(1).py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  6 00:59:21 2022
+
+@author: Giga Hertz
+"""
+
+import numpy as np
+import pickle
+
+loaded_model = pickle.load(open('E:/Python & JDK/Deploying Machine Learning model/card.sav', 'rb'))
+
+input_data = (130,0,4.89,25.98,0,72,30.42,14.71,23)
+
+input_data_as_numpy_array= np.asarray(input_data)
+
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = loaded_model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0]== 0):
+  print('The Person does not have a Coronary Heart Disease')
+else:
+  print('The Person has a Coronary Heart Disease')
